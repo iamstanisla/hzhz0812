641f3def1fb1b77e262aa24ed5644120fef588d0
iamstanisla <goduyko08@mail.ru>
2020-12-08 17:01:52 +0300

    ignoring __pycache__ packages

--- test_project/settings.py
+++ test_project/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # my application
-    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
