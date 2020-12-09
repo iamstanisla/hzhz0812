d0ade1bc55c83ddcb4ccbcc13d6de23f660c8a72
iamstanisla <goduyko08@mail.ru>
2020-12-09 19:58:11 +0300

    added a django application app

--- test_project/urls.py
+++ test_project/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path('app', include('app.urls')),
     path('admin/', admin.site.urls),
-    # path('', include('first_app.urls')),
 ]
