6e3ec1682e1aebbc809efc02787b5e7cf43f8205
iamstanisla <goduyko08@mail.ru>
2020-12-12 23:02:43 +0300

     Added path a homepage view function in list urlpatterns.

--- test_project/urls.py
+++ test_project/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app.views import homepage
+
+
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('app', include('app.urls')),
+    path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
+    path('', homepage),
 ]
