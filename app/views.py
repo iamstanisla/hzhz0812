95e2089e8c18ae08e8d27f348f13505a203cefa6
iamstanisla <goduyko08@mail.ru>
2020-12-12 08:51:57 +0300

    did fetching data and writing their database.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http.request import QueryDict
+
+from typing import Dict
 
 from .models import User
 
 
+@csrf_exempt
 def index(request):
-
-	return HttpResponse('Hello World!')
+	if request.method == 'POST':
+		data: QueryDict = request.POST
+		data_dict: Dict[str, str] = data.dict()
+		user:User = User(data_fields=data_dict)
+		user.save()
+	return render(request, 'app/index.html')
